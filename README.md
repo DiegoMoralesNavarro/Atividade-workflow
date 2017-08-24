5ded2d1a6da8d3d59439c80f7839ff97f00895cc
diego <diegomn07@gmail.com>
2017-08-24 17:26:34 -0300

    atividade

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1>Atividade-workflow</h1>
+<br>
+
+<h3>Atividade do treinamento Certificação Bootstrap, "criar um ambiente básico para desenvolver um projeto escalável".</h3>
+<br>
+
+<h3>Comandos para o terminal</h3>
+<p>Para minificar HTML: gulp html</p>
+<p>Para minificar e compilar SCSS: gulp scss</p>
---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+	htmlmin = require('gulp-htmlmin'),
+	sass = require('gulp-sass');
+
+
+
+gulp.task('html-min', () => {
+  return gulp.src('./source/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('html', () => { gulp.watch('./source/*.html', ['html-min']) })
+
+
+
+gulp.task('scss-min', () => {
+	return gulp.src('./source/scss/*.scss')
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(gulp.dest('./dist/css'))
+})
+
+gulp.task('scss', () => { gulp.watch('./source/scss/*.scss', ['scss-min']) })
+
+
+
+ /// https://www.npmjs.com/package/gulp-sass
+ /// https://www.npmjs.com/package/gulp-htmlmin/
